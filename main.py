44b90a9c1875e3f7933b6a8085560eb186e9a01d
sdm <aleksandr.tugay.02@gmail.com>
2021-10-12 19:23:47 +0300

    Timetable fix

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import requests, re, datetime
 from bs4 import BeautifulSoup
 
-#--Получаем страницу--#
 URL = 'https://www.spbume.ru/ru/viewschedule/%D0%9E%D0%94%D0%9E-%D0%9F%D0%9809-18-1/'
+DATE_PATTERN = '%d.%m.%Y'
+TOMORROW = datetime.datetime.today() + datetime.timedelta(days=1)
+TOMORROW = TOMORROW.replace(hour=0, minute=0, second=0, microsecond=0)
+TODAY = datetime.datetime.today()
+TODAY = TODAY.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
 def get(URL):
-    weekday = datetime.datetime.today().weekday()
     request = requests.get(URL)
     html = request.text
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
 
-#--Сопостовляем текущую дату с нужной неделей--#
+    return BeautifulSoup(html, 'html.parser')
+
+
 def get_date():
-    week = []
-    arr = []
-    pattern = '%d.%m.%Y'
-    date_list = []
-    today = datetime.datetime.today() + datetime.timedelta(days=1)
     soup = get(URL)
+    week = []
+    week_array = []
 
     for date in soup.find_all('span'):
         date = date.get_text('|')
@@ -29,49 +31,59 @@ def get_date():
 
     for i in week:
         i = i.split('|')
-        arr += ([[i[0], i[1]]])
+        week_array += ([[i[0], i[1]]])
 
-    counter = 1
+    week_list = []
 
-    #-Вообще, этот цикл можно выпилить, посольку почти всегда разница дней между неделями в расписании = 6, но кто знает бухгалтерию, да и почему бы не потренироваться
-    for i in arr:
-        current_date1 = datetime.datetime.strptime(i[0], pattern)
-        current_date2 = datetime.datetime.strptime(i[1], pattern)
-        days_difference = current_date2.day - current_date1.day
+    for i in week_array:
+        current_date1 = datetime.datetime.strptime(i[0], DATE_PATTERN)
+        current_date2 = datetime.datetime.strptime(i[1], DATE_PATTERN)
+        days_difference = current_date2 - datetime.timedelta(current_date1.day)
+        date_list = ([current_date1 + datetime.timedelta(days=x) for x in range(days_difference.day + 1)])
+        week_list.extend([date_list])
 
-        if days_difference < 0:
-            #--HANDMADE КоСтЫлЬ--#
-            days_difference = 6
+    return week_list
+    # for x in range(len(week_list) - 1):
+    #     try:
+    #         print(week_list[x].index(TOMORROW))
+    #         print(x)
+    #     except:
+    #         pass
 
-        date_list = ([current_date1 + datetime.timedelta(days = x) for x in range(days_difference)])
 
-        for j in date_list:
+# Получаем расписание на день
+def get_timetable(week):
+    week = 9
+    url = f"https://www.spbume.ru/ru/viewschedule/ОДО-ПИ09-18-1/{week}/"
+    timetable = [0, 1, 2, 3, 4, 5, 6, 7]
+    soup = get(url)
+    week_list = [0, 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 
-            if today.date() == j.date():
-                #--Возврат номера нужной нам недели--#
-                return counter
+    for thead in soup.find_all('thead'):
+        try:
+            if week_list.index(thead.text.strip()):
+                number = week_list.index(thead.text.strip())
+                for tbody in soup.find_all('tbody'):
+                    try:
+                        if timetable.index(tbody.text.strip()):
+                            pass
+                    except:
+                        if tbody.text.strip() == '':
+                            pass
+                        else:
+                            timetable[number] = tbody.text.strip()
+                            break
+        except:
+            pass
 
-        counter += 1
-
-#--Получаем расписание на день--#
-def get_timetable(date):
-    URL = 'https://www.spbume.ru/ru/viewschedule/%D0%9E%D0%94%D0%9E-%D0%9F%D0%9809-18-1/{0}/'.format(date)
-    print(URL)
-    timetable = []
-    soup = get(URL)
-    for headers in soup.find_all('tbody'):
-        # TODO: Реализовать алгоритм 1/0 через заполнение этого массива
-        timetable.append(headers.text.strip())
-
-    day = datetime.datetime.today().isoweekday()
-
+    day = datetime.datetime.today().weekday()
     try:
-        #--Здесь крутить значение индекса timetable для тестов--#
-        #--Значение индекса соответствует дню недели от 1 до 7--#
+        # Здесь крутить значение индекса timetable для тестов
+        # Значение индекса соответствует дню недели от 1 до 7
         if len(timetable) < 7:
-            print(timetable[day-1])
+            print(timetable[day - 1])
         else:
-            print(timetable[day+1])
+            print(timetable[day + 1])
     except:
         print('Выходной')
 
